c4ed062bac1c86db8d438c4919c3b6905e9b867e
Sascha <radatz.coding@gmail.com>
2019-03-27 21:56:28 +0800

    init

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+var config = require("./config");
+var webservice = require("./webservice");
+var bleserver = require("./bleserver");
 
+bleserver.init();
 
 // curl --header "Content-Type: application/json" --request POST --data '{"username":"xyz","password":"xyz"}' http://mbpro.local:3000/maketx
