1aa169049fc0934b225f2f4dc29f8a06ea4c1011
Sascha <radatz.coding@gmail.com>
2019-03-27 22:38:43 +0800

    quick config change

---
+++ index_car.js
@@ -0,0 +1,9 @@
+var config = require("./config_car.json");
+var webservice = require("./webservice");
+
+var bleserver = require("./bleserver");
+var blescanner = require("./blescanner");
+
+blescanner.init();
+
+// curl --header "Content-Type: application/json" --request POST --data '{"username":"xyz","password":"xyz"}' http://mbpro.local:3000/maketx
