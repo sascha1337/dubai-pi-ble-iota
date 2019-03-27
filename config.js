f95d1d59fc5eea94d72d0ae63a249f24546a3be9
Sascha <radatz.coding@gmail.com>
2019-03-27 22:03:35 +0800

    ble name fix

--- config.js
+++ config.js
@@ -4,12 +4,13 @@ var device = {};
 switch (config.mode) {
     case "device_car":
         device = config.device_car;
+        process.env['BLENO_DEVICE_NAME'] = config.device_car.ble_name;
         break;
 
     case "device_parking":
         device = config.device_parking;
+        process.env['BLENO_DEVICE_NAME'] = config.device_parking.ble_name;
         break;
-
     default:
         console.error("MODE ERROR; CHECK CONFIG");
         break;
