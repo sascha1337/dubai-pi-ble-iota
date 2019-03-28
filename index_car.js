d91712bfb4593ec0830a1d1ac02cc84677c09c93
Sascha <radatz.coding@gmail.com>
2019-03-29 04:35:41 +0800

    ble adv / scan same time test

--- index_car.js
+++ index_car.js
@@ -3,8 +3,11 @@ process.env['BLENO_DEVICE_NAME'] = config.ble_name;
 
 var webservice = require("./webservice");
 var blescanner = require("./blescanner");
+var bleserver = require("./bleserver");
 
 blescanner.init(config);
+bleserver.init(config);
+
 webservice.init(config);
 
 // curl --header "Content-Type: application/json" --request POST --data '{"username":"xyz","password":"xyz"}' http://mbpro.local:3000/maketx
