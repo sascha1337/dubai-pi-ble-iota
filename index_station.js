2c72a15b7cfce0c26d580531777a09ad4cb5f666
Sascha <radatz.coding@gmail.com>
2019-03-29 04:49:48 +0800

    up

--- index_station.js
+++ index_station.js
@@ -2,9 +2,9 @@ var config = require("./config_parking.json");
 process.env['BLENO_DEVICE_NAME'] = config.ble_name;
 
 var webservice = require("./webservice");
-var bleserver = require("./bleserver");
+var blescanner = require("./blescanner");
 
-bleserver.init(config);
+blescanner.init(config);
 webservice.init(config);
 
 // curl --header "Content-Type: application/json" --request POST --data '{"username":"xyz","password":"xyz"}' http://mbpro.local:3000/maketx
